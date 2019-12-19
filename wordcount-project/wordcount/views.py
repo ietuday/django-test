a9842c3fa1bda4035bbdabf08702dbfa4b4649af
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-12-19 19:44:54 +0530

    "changes"

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
+
+
+def count(request):
+    fullText = request.GET['fullText']
+    wordList = fullText.split()
+    worddictionary = {}
+    for word in wordList:
+        if word in worddictionary:
+            # Increase Count
+            worddictionary[word] += 1
+        else:
+            # Add to the dictionary
+            worddictionary[word] = 1
+    return render(request, 'count.html', {'fullText': fullText, 'count': len(wordList), 'worddictionary': worddictionary.items()})
