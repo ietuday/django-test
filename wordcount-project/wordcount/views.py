cb1f8ed0265225fb57cffb3b902773f1216ad83a
Udayaditya Singh <udayaditya.singh@gmail.com>
2019-12-20 12:23:37 +0530

    "changes"

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 
 def home(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
+
+
+def about(request):
+    return render(request, 'about.html')
 
 
 def count(request):
@@ -17,4 +22,6 @@ def count(request):
         else:
             # Add to the dictionary
             worddictionary[word] = 1
-    return render(request, 'count.html', {'fullText': fullText, 'count': len(wordList), 'worddictionary': worddictionary.items()})
+    sortedwords = sorted(worddictionary.items(),
+                         key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fullText': fullText, 'count': len(wordList), 'sortedwords': sortedwords})
